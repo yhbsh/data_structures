bf5b699618b81865541752d012886767ba919bc1
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-11-28 12:29:11 +0300

    feat: Add list_dump function and update makefile

--- linked_list/main.c
+++ linked_list/main.c
@@ -1,47 +1,7 @@
+#include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-struct node {
-    struct node *next;
-    int value;
-};
-
-void list_insert(struct node **head, int value) {
-    struct node *node = malloc(sizeof(struct node));
-    node->value = value;
-    node->next = NULL;
-
-    if (*head == NULL) {
-        *head = node;
-        return;
-    }
-
-    struct node *temp;
-    for (temp = *head; temp->next != NULL; temp = temp->next)
-        ;
-
-    temp->next = node;
-}
-
-void list_print(struct node *head) {
-    if (head != NULL) {
-        printf("%d ", head->value);
-    }
-
-    for (struct node *temp = head->next; temp != NULL; temp = temp->next) {
-        printf("-> %d ", temp->value);
-    }
-    printf("\n");
-}
-
-void list_free(struct node *head) {
-    if (head == NULL)
-        return;
-
-    for (struct node *temp = head->next, *prev = head; temp != NULL; prev = temp, temp = temp->next)
-        free(prev);
-}
 
 int main(void) {
     srand(time(0));
@@ -51,7 +11,7 @@ int main(void) {
         list_insert(&head, rand() % 200);
     }
 
-    list_print(head);
+    list_dump(head);
 
     list_free(head);
 
