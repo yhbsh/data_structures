61be9f0ab51cf9aec69fc55d743c982ae42d064e
HoussemBousmaha <houssembousmaha.ksu@gmail.com>
2024-01-28 03:46:58 +0300

    format

--- doubly_linked_list/ddl.c
+++ doubly_linked_list/ddl.c
@@ -1,58 +1,55 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define COUNT 5
-#define MAX		300
+#define MAX 300
 
 struct node_t {
-	struct node_t *next;
-	struct node_t *prev;
-	int data;
+  struct node_t *next;
+  struct node_t *prev;
+  int data;
 };
 
-
 int main(void) {
-	struct node_t *head, *temp;
+  struct node_t *head, *temp;
 
-	head = (struct node_t *)malloc(sizeof(struct node_t));
-	head->data = rand() % MAX;
-	head->next = NULL;
-	head->prev = NULL;
+  head = (struct node_t *)malloc(sizeof(struct node_t));
+  head->data = rand() % MAX;
+  head->next = NULL;
+  head->prev = NULL;
 
-	temp = head;
+  temp = head;
 
-	for (int i = 0; i < COUNT; i++) {
-		struct node_t *node = (struct node_t *)malloc(sizeof(struct node_t));
+  for (int i = 0; i < COUNT; i++) {
+    struct node_t *node = (struct node_t *)malloc(sizeof(struct node_t));
 
-		node->data = rand() % MAX;
-		node->next = NULL;
-		
-		temp->next = node;
-		node->prev = temp;
-		temp = node;
-	}
+    node->data = rand() % MAX;
+    node->next = NULL;
 
-	FILE *f = fopen("main.dot", "wb");
+    temp->next = node;
+    node->prev = temp;
+    temp = node;
+  }
 
-	fprintf(f, "digraph G {\n");
-	fprintf(f, "\tsize     =\t\"6.6\"   ;\n");
-	fprintf(f, "\trankdir  =\t\"LR\"    ;\n");
+  FILE *f = fopen("main.dot", "wb");
 
-	
-	for (struct node_t *temp = head; temp->next != NULL; temp = temp->next) {
-		fprintf(f, "\t%-7d\t->\t%-8d;\n", temp->data, temp->next->data);
-	}
+  fprintf(f, "digraph G {\n");
+  fprintf(f, "\tsize     =\t\"6.6\"   ;\n");
+  fprintf(f, "\trankdir  =\t\"LR\"    ;\n");
 
-	fprintf(f, "}");
-	fclose(f);
+  for (struct node_t *temp = head; temp->next != NULL; temp = temp->next) {
+    fprintf(f, "\t%-7d\t->\t%-8d;\n", temp->data, temp->next->data);
+  }
 
-	system("dot -Tjpg -o main.jpg main.dot");
-	system("open main.jpg");
+  fprintf(f, "}");
+  fclose(f);
 
-	while(temp != NULL) {
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+  system("dot -Tjpg -o main.jpg main.dot");
+  system("open main.jpg");
 
+  while (temp != NULL) {
+    temp = head->next;
+    free(head);
+    head = temp;
+  }
 }
